0c63a23264053c939303ae0991053cbac86963e2
nodichao <josephhenrinodichao@gmail.com>
2024-05-21 18:51:35 +0200

    adding options

--- elek-consulting-group-backend/config/dbConfig.js
+++ elek-consulting-group-backend/config/dbConfig.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI).then(()=>{
+mongoose.connect(MONGO_URI,{
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+   }).then(()=>{
      console.log('Connection to database successful');
 }).catch((err)=>{
        console.error(err);
