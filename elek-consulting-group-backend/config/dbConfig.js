e45288a36169c517fa8d9e0c2953551f8627004e
nodichao <josephhenrinodichao@gmail.com>
2024-05-21 19:07:06 +0200

    removing options

--- elek-consulting-group-backend/config/dbConfig.js
+++ elek-consulting-group-backend/config/dbConfig.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI,{
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-   }).then(()=>{
+mongoose.connect(MONGO_URI).then(()=>{
      console.log('Connection to database successful');
 }).catch((err)=>{
        console.error(err);
