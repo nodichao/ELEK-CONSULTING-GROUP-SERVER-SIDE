3008f6188ac1e37720c20d2b570758e65d0f2ef5
nodichao <josephhenrinodichao@gmail.com>
2024-05-21 23:33:38 +0200

    try with vercel

--- elek-consulting-group-backend/config/dbConfig.js
+++ elek-consulting-group-backend/config/dbConfig.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-//const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect("mongodb+srv://joseph:101998joe@cluster0.thxuyos.mongodb.net/elek_consulting_group",{
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-   }).then(()=>{
+mongoose.connect(MONGO_URI).then(()=>{
      console.log('Connection to database successful');
 }).catch((err)=>{
        console.error(err);
