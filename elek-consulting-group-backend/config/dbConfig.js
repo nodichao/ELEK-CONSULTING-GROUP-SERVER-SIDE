74c26cfdb61a4bf3dd139c516855b79c4f686879
nodichao <josephhenrinodichao@gmail.com>
2024-05-21 19:23:31 +0200

    adding new options

--- elek-consulting-group-backend/config/dbConfig.js
+++ elek-consulting-group-backend/config/dbConfig.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 //const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect("mongodb+srv://joseph:101998joe@cluster0.thxuyos.mongodb.net/elek_consulting_group").then(()=>{
+mongoose.connect("mongodb+srv://joseph:101998joe@cluster0.thxuyos.mongodb.net/elek_consulting_group",{
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+   }).then(()=>{
      console.log('Connection to database successful');
 }).catch((err)=>{
        console.error(err);
